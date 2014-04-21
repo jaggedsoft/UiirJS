5328364bb462aafd00ee6dfad8674415e87a6304
Obviously Sorna <johnbptst@yahoo.com>
2014-04-21 12:47:16 -0500

    Not totally sure.
    This is what happens when you develop from two different places -- you
    forget what it is you changed on the machine that has your source
    control on it, while you develop elsewhere from a Surface RT which
    doesn't. :/

--- app/uiir.map.js
+++ app/uiir.map.js
@@ -1,8 +1,9 @@
 var uiir = uiir || {};
 
-/*--------=### Map View Model ###=---------*/
+/*--------=### Map Class ###=---------*/
 //
 //    .
 //    .
 //
 //-----------------------------------------//
+// NOTE:  Should take advantage of prototyping so that shared functions aren't repeated on a per-map basis.
---
+++ app/uiir.terrain.js
@@ -0,0 +1,10 @@
+var uiir = uiir || {};
+
+/*--------=### Terrain class ###=---------*/
+//
+//    .
+//    .
+//
+//-----------------------------------------//
+// NOTE:  Should take advantage of prototyping so that shared functions aren't repeated on a per-terrain basis.
+// NOTE:  Might want to see if there's anything here that could be shared with mob, object, or player and separate it into a base
